fee0b93c20730c2bd9acb24fc7d14b2fdfeee05f
Donia-F <104778302+Donia-F@users.noreply.github.com>
2022-05-04 17:30:50 -0400

    Add files via upload

---
+++ registrazione/formValidation.js
@@ -0,0 +1,17 @@
+function validaForm() {
+    if (document.myForm.remember.checked) {
+        window.alert("Hai scelto di ricordarti per i prossimi accessi" );
+    }
+    else {
+        window.alert("Hai scelto di non ricordarti per i prossimi accessi" );
+    }
+    if(document.myForm.inputName.value==""){
+        alert("Inserire nome");
+        return false;
+    }
+    if(document.myForm.inputEmail.value==""){
+        alert("Inserire email");
+        return false;
+    }
+
+}
---
+++ registrazione/registrazione.php
@@ -0,0 +1,30 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+        $dbconn = pg_connect( "host=localhost port=5433 dbname=Registrazione user=postgres password=password ")
+            or die( 'Could not connect: ' . pg_last_error());
+        if ( !( isset( $_POST [ ' registrationButton ' ] ) ) ) {
+            header ( " Location : ../index.html" );
+        }else {
+            $email = $_POST [ ' inputEmail ' ] ;
+            $q1= "select ∗ from utente where email= $1";
+            $result=pg_query_params($dbconn, $q1, array($email));
+            if($line=pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                echo "<h1> Sorry, you are already a registered user </h1>
+                    <a href = ../login/index.html> Click here to login </a>";
+                }
+            else {
+                $nome = $_POST [ ' inputName ' ] ;
+                $password = md5( $_POST [ ' inputPassword ' ] ) ;
+                $q2 = " insert into utente values ( $1 , $2 , $3 , $4 , $5 ) " ;
+                $data = pg_query_params ( $dbconn, $q2, array ( $email, $nome, $password ) ) ;
+                    if( $data ) {
+                        echo "<h1> Registrazione completata<br/></h1>";
+                        echo "<a href = ../Welcome.php? name=$nome>Premi qui per inziare ad utilizzare il sito web </a>";
+                    }
+                }
+            }
+        ?>
+    </ body>
+</ html>
