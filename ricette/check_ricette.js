f7446373b234ccf7ffae4bf03003b00a242c4e8e
cooking-forum <104777453+cooking-forum@users.noreply.github.com>
2022-05-05 09:19:26 +0200

    Add files via upload

---
+++ ricette/check_ricette.js
@@ -0,0 +1,17 @@
+function checkRicetta() {
+    if (document.myRicetta.nomeRicetta.value == "") {
+        window.alert("Inserire nome ricetta!");
+    }
+
+    if (document.myRicetta.nomeAutore.value == "") {
+        window.alert("Inserire nome autore!");
+    }
+
+    if (document.myRicetta.ingredienti.value == "") {
+        window.alert("Inserire ingredienti!");
+    }
+
+    if (document.myRicetta.nomeRicetta.value == "") {
+        window.alert("Inserire descrizione del procedimento!");
+    }
+}
