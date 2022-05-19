58dc55b526e9d3dfd17bdaa3fcf892228f69b45f
Donia-F <104778302+Donia-F@users.noreply.github.com>
2022-05-19 09:12:32 -0400

    Add files via upload

--- ricette/form-ricette.php
+++ ricette/form-ricette.php
@@ -3,45 +3,72 @@
 <head></head>
 <body>
     <?php
-        $dbconn = pg_connect("host=postgresql_database port=5432 dbname=test user=admin password=admin1234")
+        session_start();
+        $utente=$_SESSION['username'];
+       
+
+        $dbconn = pg_connect("host=localhost port=5432 dbname=registrazione user=donia password=diag")
                         or die("Impossibile connettersi: " . pg_last_error());
 
         if(!(isset($_POST["submitButton"] ) ) ) {
             header ( " Location : form-ricette.html" );
         }
         else {
-            $ricetta = $_POST["nomeRicetta"];
-            $q1 = "SELECT * from ricetta where nomer = $ricetta";
-            $result = pg_query_params($dbconn, $q1, array($ricetta));
+            $nomer = $_POST["nomeRicetta"];
+            $q1 = 'SELECT * from ricetta where nomer=$1';
+            $result = pg_query_params($dbconn, $q1, array($nomer));
             if ($line=pg_fetch_array($result, null, PGSQL_ASSOC)) {
                 echo "<h1> Una ricetta con lo stesso nome è già stata inserita! Prova a aggiungere un commento sotto al post <h1>";
             }
             else {
-                $autore = $_POST["nomeAutore"];
-                $proc = $_POST["procedimento"];
+                
                 $tipo = $_POST["tipo"];
-                // $foto = $_POST["caricaFoto"];
-                $q2 = "INSERT into ricetta (utente, nomer, procedimento, tipologia) values ('$autore', '$ricetta', '$proc', '$tipo')";
-                $data2 = pg_query($dbconn, $q2);
+                $proc = $_POST["procedimento"];
+                $ingr = $_POST["ingredienti"];
+                
 
-                if ($data2) {
-                    echo "<h1> Ricetta inserita con successo!! <br></h1>";
+
+                // foto
+                $upload_dir=getcwd().DIRECTORY_SEPARATOR.'/uploads';
+                if($_FILES["caricaFoto"]["error"]==UPLOAD_ERR_OK){
+                    $temp_name=$_FILES["caricaFoto"]["tmp_name"];
+                    $name=basename($_FILES["caricaFoto"]["name"]);
+                    $save_path=$upload_dir.$name;
+                    move_uploaded_file($temp_name,$save_path);
+                    $uploaded=true;
+    
+                }
+                if($uploaded){
+                    $fh=fopen($save_path,'rb');
+                    $fbytes=fread($fh,filesize($save_path));
+    
+                    if($dbconn){
+                        $query="call save_fotoricette($1,$2)";
+                        $res=pg_query_params($dbconn,$query,array(base64_encode($fbytes),"$nomer"));
+                        if($res){
+                            echo 'saved'.strlen(base64_encode($fbytes));
+                        }
+                    }
                 }
                 
-                $ingr = $_POST["ingrediente1"];
-                $nume = $_POST["numero1"];
-                $unit = $_POST["unita1"];
-                $q3 = "INSERT into ingrediente (ricetta, nomei, quantita, unita) values ('$ricetta', '$ingr', '$nume', '$unit')";
-                $data3 = pg_query($dbconn, $q3);
-            
-                if ($data3) {
-                    echo "<h1> ingredienti inseriti con successo!! <br></h1>";
+
+
+
+
+                $q2 = 'INSERT into ricetta values ($1,$2,$3,$4,$5)' ;
+                $data2 = pg_query_params($dbconn, $q2, array($nomer,$utente,$tipo,$proc,$ingr));
+
+                if ($data2) {
+                    echo "Ricetta inserita con successo!!";
                 }
+                
+                
+              
             }
         }
+    
     ?>
     
     
 </body>
 </html>
-
